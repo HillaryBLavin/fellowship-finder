357bb7217132d7ee75bb83a3b9d0a4854f98428b
Hillary Lavin <hilavin@gmail.com>
2018-05-11 14:48:48 -0400

    Remove jsonParser and urlencodedParser

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var PORT = process.env.PORT || 8080;
 
-var jsonParser = bodyParser.json()
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
@@ -17,7 +16,9 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
 
+require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
